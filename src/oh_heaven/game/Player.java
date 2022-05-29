7dc01414621f1cb86bf98dd3793359af136da751
C4r50nC <xcch7@student.unimelb.edu.au>
2022-05-29 17:15:00 +0800

    Deleted unused imports

--- src/oh_heaven/game/Player.java
+++ src/oh_heaven/game/Player.java
@@ -1,20 +1,10 @@
 package oh_heaven.game;
 
-import ch.aplu.jcardgame.*;
-import ch.aplu.jgamegrid.*;
-import java.awt.Color;
-import java.awt.Font;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Player extends CardGame {
+public class Player {
     private int score = 0;
     private int trick = 0;
     private int bid = 0;
     private boolean isHuman;
-    private Actor scoreActor = null;
-
-    Font bigFont = new Font("Serif", Font.BOLD, 36);
 
     public int getScore() {
         return score;
